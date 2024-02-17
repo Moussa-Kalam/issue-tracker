72d09d9ac0be5a434ea100adfc5cb8a3a2768357
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-17 19:28:15 +0200

    style: customize theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
+import { Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
-import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import NavBar from './NavBar';
-import { Theme } from '@radix-ui/themes';
+import './globals.css';
+import './theme-config.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,8 +20,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} text-slate-950`}>
-        <Theme>
+      <body className={`${inter.variable} text-slate-950`}>
+        <Theme appearance='light' accentColor='green'>
           <NavBar />
           <main className='p-5'>{children}</main>
         </Theme>
