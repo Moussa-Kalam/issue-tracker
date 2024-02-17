77d76462b49c13aadfa4ca0fb73cd813f98b5a3d
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-17 19:04:56 +0200

    chore: setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@radix-ui/themes';
 
 const IssuesPage = () => {
-  return (
-    <div>IssuesPage</div>
-  )
-}
+  return <Button>New Issue</Button>;
+};
 
-export default IssuesPage
+export default IssuesPage;
