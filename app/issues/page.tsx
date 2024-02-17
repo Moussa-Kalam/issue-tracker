5d5db83627373598495b0d96867cdc4390cc8300
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-17 19:13:59 +0200

    feat: build new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 
 const IssuesPage = () => {
-  return <Button>New Issue</Button>;
+  return (
+    <>
+      <Button>
+        <Link href='/issues/new'>New Issue</Link>
+      </Button>
+    </>
+  );
 };
 
 export default IssuesPage;
