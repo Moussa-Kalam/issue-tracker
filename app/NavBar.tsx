5a93b11ca01b1c6f5d173003194894f71c216588
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-17 18:10:30 +0200

    style: highlight active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
+import classNames from 'classnames';
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' },
@@ -17,7 +23,11 @@ const NavBar = () => {
         {links.map((link) => (
           <li
             key={link.href}
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            className={classNames({
+              'text-zinc-900': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
           >
             <Link href={link.href}>{link.label}</Link>
           </li>
