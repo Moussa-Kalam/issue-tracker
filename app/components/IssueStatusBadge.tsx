df7eb4d0e054fbdd886afff138d81ad7788ba968
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-19 01:09:55 +0200

    feat: build the issue status badge

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,20 @@
+import { Status } from '@prisma/client';
+import { Badge } from '@radix-ui/themes';
+import React from 'react';
+
+const statusMap: Record<
+  Status,
+  { label: string; color: 'red' | 'blue' | 'green' }
+> = {
+  OPEN: { label: 'Open', color: 'red' },
+  IN_PROGRESS: { label: 'In Progress', color: 'blue' },
+  CLOSED: { label: 'Closed', color: 'green' },
+};
+
+const IssueStatusBadge = ({ status }: { status: Status }) => {
+  return (
+    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+  );
+};
+
+export default IssueStatusBadge;
