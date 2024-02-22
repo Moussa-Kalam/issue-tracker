6d4afb8609543c41099a13ada882ced043c04eca
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-22 20:33:01 +0200

    feat: build an api for updating issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,36 @@
+import { issueSchema } from '@/app/validationSchemas';
+import { NextResponse } from 'next/server';
+import prisma from '@/prisma/client';
+
+export const PATCH = async (
+  request: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) => {
+  const body = await request.json();
+
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: 'Invalid issue' }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue, { status: 200 });
+};
