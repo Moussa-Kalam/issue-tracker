e3caf24d4cd53baeba6b7b8ddb19901d04b15198
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-22 21:31:16 +0200

    feat: add a delete button

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -11,7 +11,7 @@ const IssueDetails = ({ issue }: { issue: Issue }) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
+      <Card className='prose max-w-full'>
         <ReactMarkdown className='prose mt-4'>
           {issue.description}
         </ReactMarkdown>
