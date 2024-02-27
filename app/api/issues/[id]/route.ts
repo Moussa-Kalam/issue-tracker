2773475ca7275d1915551f33b9d3a4140e03baf1
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-27 22:59:20 +0200

    feat: build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -34,3 +34,25 @@ export const PATCH = async (
 
   return NextResponse.json(updatedIssue, { status: 200 });
 };
+
+export const DELETE = async (
+  request: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) => {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: 'Invalid issue' }, { status: 404 });
+
+  await prisma.issue.delete({
+    where: {
+      id: issue.id,
+    },
+  });
+
+  return NextResponse.json({});
+};
