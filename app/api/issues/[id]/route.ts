d91e616b29b877a50f077388343170bc86694121
Moussa-Kalam <m.amzat@alustudent.com>
2024-03-01 17:43:29 +0200

    feat: secure the application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,11 +1,16 @@
+import authOptions from '@/app/auth/authOption';
 import { issueSchema } from '@/app/validationSchemas';
 import prisma from '@/prisma/client';
+import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const PATCH = async (
   request: NextRequest,
   { params: { id } }: { params: { id: string } }
 ) => {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const body = await request.json();
 
   const validation = issueSchema.safeParse(body);
@@ -39,6 +44,9 @@ export const DELETE = async (
   request: NextRequest,
   { params: { id } }: { params: { id: string } }
 ) => {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const issue = await prisma.issue.findUnique({
     where: {
       id: parseInt(id),
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -5,12 +5,16 @@ import { notFound } from 'next/navigation';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
 import DeleteIssueButton from './DeleteIssueButton';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/auth/authOption';
 
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailpage = async ({ params: { id } }: Props) => {
+  const session = await getServerSession(authOptions);
+
   if (typeof parseInt(id) !== 'number') notFound();
 
   const issue = await prisma.issue.findUnique({
@@ -21,19 +25,19 @@ const IssueDetailpage = async ({ params: { id } }: Props) => {
 
   if (!issue) notFound();
 
-  await delay(2000);
-
   return (
     <Grid columns={{ initial: '1', sm: '5' }} gap='5'>
       <Box className='md:col-span-4'>
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <Flex direction='column' gap='4'>
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction='column' gap='4'>
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 };
