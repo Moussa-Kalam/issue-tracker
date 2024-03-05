3523435e091976bd36da3f947394794937006575
Moussa-Kalam <m.amzat@alustudent.com>
2024-03-05 20:54:36 +0200

    feat: filter issues based on status

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,12 +1,24 @@
 import prisma from '@/prisma/client';
 import { Table, TableHeader, TableRoot } from '@radix-ui/themes';
-import delay from 'delay';
 import { IssueStatusBadge, Link } from '@/app/components';
 import IssueActions from './IssueActions';
+import { Status } from '@prisma/client';
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
-  await delay(2000);
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
 
   return (
     <>
