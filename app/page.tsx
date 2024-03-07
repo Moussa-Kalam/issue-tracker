dafe82812f61dfe17e3e289c14b617f88e9ffacd
Moussa-Kalam <m.amzat@alustudent.com>
2024-03-08 00:52:26 +0200

    feat: paginate the issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
 import Pagination from './components/Pagination';
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default function Home() {
   return (
-    <>
-      <main className='text-3xl font-semibold text-black'>
-        <h1>Welcome to the issue tracker</h1>
-      </main>
-      <Pagination
-        pageSize={10}
-        itemCount={100}
-        currentPage={parseInt(searchParams.page)}
-      />
-    </>
+    <main className='text-3xl font-semibold text-black'>
+      <h1>Welcome to the issue tracker</h1>
+    </main>
   );
 }
